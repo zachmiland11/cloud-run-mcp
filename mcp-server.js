ea4c6f3faf050715d1478621e2b9e4d1a32bdc14
Steren <steren.giannini@gmail.com>
2025-05-05 23:19:52 -0700

    wire MCP server

--- mcp-server.js
+++ mcp-server.js
@@ -3,7 +3,8 @@ import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
 import { StreamableHTTPServerTransport } from "@modelcontextprotocol/sdk/server/streamableHttp.js";
 // Support SSE for backward compatibility
 import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
-import { deploy } from './deploy.js';
+import { z } from "zod";
+import { deploy } from './deploy-cloud-run.js';
 
 const getServer = () => {
   // Create an MCP server with implementation details
@@ -13,58 +14,58 @@ const getServer = () => {
   }, { capabilities: { logging: {} } });
 
 
-  // Register a tool specifically for testing resumability
+  // Register the deploy tool
   server.tool(
     'deploy',
-    'Deploy files to Cloud Run',
     {
-      project: { type: 'string', description: 'Project ID' },
-      region: { type: 'string', description: 'Region to deploy the service to', default: 'europe-west1' },
-      service: { type: 'string', description: 'Name of the Cloud Run service to deploy to' },
-      files: { type: 'array', description: 'Files to deploy', items: { type: 'string' } },
+      project: z.string().describe('Google Cloud project ID'),
+      region: z.string().default('europe-west1').describe('Region to deploy the service to'),
+      service: z.string().default('app').describe('Name of the Cloud Run service to deploy to'),
+      files: z.array(z.string()).describe('Files to deploy (provided as file path on the local filesystem)'),
     },
     async ({ project, region, service, files }) => {
+      console.log(`New deploy request: ${JSON.stringify({ project, region, service, files })}`);
+
+      // TODO: if remote MCP, just deploy in the same project as the MCP server
       if (typeof project !== 'string') {
-        throw new Error('Project must specified');
+        throw new Error('Project must specified, please prompt the user for a valid existing Google Cloud project ID.');
       }
-      if (typeof service !== 'string') {
-        throw new Error('Service must specified');
-      }
-      if (typeof files !== 'array') {
+      if (typeof files !== 'object' || !Array.isArray(files)) {
         throw new Error('Files must specified');
       }
-
-      const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-      let counter = 0;
-
-      while (count === 0 || counter < count) {
-        counter++;
-        try {
-          await sendNotification({
-            method: "notifications/message",
-            params: {
-              level: "info",
-              data: `Periodic notification #${counter} at ${new Date().toISOString()}`
-            }
-          });
-        }
-        catch (error) {
-          console.error("Error sending notification:", error);
-        }
-        // Wait for the specified interval
-        await sleep(interval);
+      if (files.length === 0) {
+        throw new Error('No files specified for deployment');
       }
 
-      return {
-        content: [
-          {
-            type: 'text',
-            text: `Started sending periodic notifications every ${interval}ms`,
-          }
-        ],
-      };
-    }
-  );
+      // Deploy to Cloud Run
+      try {
+        // TODO: Should we return intermediate progress messages?
+        await deploy({
+          projectId: project,
+          serviceName: service,
+          region: region,
+          files: files,
+        });
+        return {
+          // TODO: return URL to the deployed service
+          content: [
+            {
+              type: 'text',
+              text: `Files deployed to Cloud Run service ${service} in project ${project}`,
+            }
+          ],
+        };
+      } catch (error) {
+        return {
+          content: [
+            {
+              type: 'text',
+              text: `Error deploying to Cloud Run: ${error}`,
+            }
+          ],
+        };
+      }
+    });
 
   // TODO services as resources
   return server;
@@ -74,6 +75,7 @@ const app = express();
 app.use(express.json());
 
 app.post('/mcp', async (req, res) => {
+  console.log('/mcp Received:', req.body);
   const server = getServer();
   try {
     const transport = new StreamableHTTPServerTransport({
@@ -130,6 +132,7 @@ const sseTransports = {};
 
 // Legacy SSE endpoint for older clients
 app.get('/sse', async (req, res) => {
+  console.log('/sse Received:', req.body);
   const server = getServer();
   // Create SSE transport for legacy clients
   const transport = new SSEServerTransport('/messages', res);
@@ -144,6 +147,7 @@ app.get('/sse', async (req, res) => {
 
 // Legacy message endpoint for older clients
 app.post('/messages', async (req, res) => {
+  console.log('/messages Received:', req.body);
   const sessionId = req.query.sessionId;
   const transport = sseTransports[sessionId];
   if (transport) {
@@ -156,7 +160,7 @@ app.post('/messages', async (req, res) => {
 // Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`MCP Stateless Streamable HTTP Server listening on port ${PORT}`);
+  console.log(`Cloud Run MCP server listening on port ${PORT}`);
 });
 
 // Handle server shutdown
